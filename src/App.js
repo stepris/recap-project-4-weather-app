a63df49d4a476f45c3ad11d384c0a9a70741770f
stepris <122841614+stepris@users.noreply.github.com>
2024-06-28 13:13:41 +0200

    Clean up form handler

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 function App() {
-  /*  const [name, setName] = useState("");
-  const [isForGoodWeather, setIsForGoodWeather] = useState(false); */
-  /* 
-  function handleAddActivity(data) {
-    // setName(data.name);
-    // setIsForGoodWeather(data.isForGoodWeather.checked);
-    // console.log("data", data.isForGoodWeather.checked);
-    // console.log("dataFromUseState", isForGoodWeather);
-    // console.log("dataFromForm", data);
-  } */
-  // console.log("nameFromUseState", name);
-  // console.log(isForGoodWeather);
+  const [activity, setActivity] = useState("");
+
+  function handleAddActivity(newActivity) {
+    setActivity(newActivity);
+  }
+
+  console.log(activity);
+
   return (
     <div className="App">
       <p>Welcome to our new weather app</p>
-      {/*     <Form onAddActivity={handleAddActivity} /> */}
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,17 @@
 export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
-    const data = {
-      isForGoodWeather: event.target.isForGoodWeather.checked,
-      name: event.target.name.value,
-    };
-    // console.log(event.target.name.value);
-    console.dir(event.target.isForGoodWeather);
-    console.log("Self built object:", data);
 
-    // const dataForm = new FormData(event.target);
-    // const data = Object.fromEntries(dataForm);
-    // console.log("Data Object before:", data);
-    // data.isForGoodWeather = event.target.isForGoodWeather.checked;
-    // console.log("Data Object before:", data);
-    // console.dir(event.target);
-    // console.log("Event:", event);
-    // console.log("Event target:", event.target);
-    // console.log("Checkbox: ", event.target.isForGoodWeather.checked);
-    // onAddActivity(data);
+    const formData = new FormData(event.target);
+
+    const newActivity = {
+      isForGoodWeather: event.target.isForGoodWeather.checked,
+      name: formData.get("name"),
+    };
+
+    onAddActivity(newActivity);
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <h1>Add new Activity</h1>
