629cf9882e7d1041ee44e706485ba981d53f6881
Fathimath <faseela863gqqQq@@gmail.com>
2024-06-28 11:26:23 +0200

    start building out form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import { Form } from "./components/Form";
 function App() {
   return (
     <div className="App">
       <p>Welcome to our new weather app</p>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+export function Form() {
+  return (
+    <form>
+      <h1>Add new Activity</h1>
+      <p>hai</p>
+      <label htmlFor="nameOfActivity">Name:</label>
+      <input type="text" id="nameOfActivity" name="nameOfActivity" />
+    </form>
+  );
+}
