486cd4b87104a169bb90b42caf2a36895338811c
Fathimath <faseela863gqqQq@@gmail.com>
2024-06-28 15:08:38 +0200

    add uid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import { uid } from "uid";
 function App() {
   const [activities, setActivities] = useState([]);
 
   function handleAddActivity(newActivity) {
-    setActivities([...activities, newActivity]);
-  }
+    //st newAct  newActivity.id = uid();
 
+    // console.log(newActivityWithId);
+
+    //console.log(newActivity);
+    setActivities([
+      ...activities,
+      {
+        id: uid(),
+        ...newActivity,
+      },
+    ]);
+  }
+  console.log(activities);
   return (
     <div className="App">
       <p>Welcome to our new weather app</p>
